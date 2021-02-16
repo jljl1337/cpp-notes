258c2181ff4858df36feb96c76a9a38efe4b16ff
Jack Leung <lckjack123@gmail.com>
2021-02-17 02:39:18 +0800

    Shorten comments within the 80 character range

--- notes.cpp
+++ notes.cpp
@@ -27,7 +27,8 @@ int main() {
     number = 3; // initialisation or definition
     // MUST declare before define the variable
 
-    // The variable name is case sensitive, which means "A" is differnt from "A"
+    // The variable name is case sensitive, 
+    // which means "A" is differnt from "A"
     // Only alphabet, number and "_" can be used,
     // such as "billy_age", but other cannot, so "billy-age" is a invalid name
 
@@ -40,17 +41,20 @@ int main() {
     short short_integer = 2; // integer (2 bytes / 16 bits)
     long long_integer = 8; // integer (8 bytes / 64 bits)
     unsigned int unsigned_integer = 3; // integer with min of 0 and doubled max
-    // Overflow error may occur without warning when exceed max or is below max value
+    // Overflow error may occur without warning 
+    // when exceed max or is below max value
 
     float floating_point_number = 3.9; // real number (4 bytes / 32 bits)
     double double_number = 3e-2; // default real number (8 bytes / 64 bits)
 
     // Character
-    char character = 'd'; // single character of ASCII code (MUST use '', 1 bytes / 8 bits)
+    char character = 'd'; // single character of ASCII code 
+    // (MUST use '', 1 bytes / 8 bits)
     cout << character + 1 << endl; // 100 (ASCII of 'd') + 1 = 101
     character = character + 1;
     cout << character << endl; // 'e'
-    // '\' is a escape character, which can help writing special character like tab or "
+    // '\' is a escape character, which can help writing special character,
+    // like tab or "
 
     // String
     char string1[] = "amy";
@@ -82,12 +86,14 @@ int main() {
         // x can be accessed here
     }
     // x cannot be accessed here
-    // with namespace used (in global scope), variables inside can be accessed outside,
+    // with namespace used (in global scope), 
+    // variables inside can be accessed outside,
     // like this: namespace level 1 { int x = 10; } 
 
 
     // sizeof() operator
-    // this will return the memory space used of a variable or a type of variable
+    // this will return the memory space used of a variable 
+    // or a type of variable
     cout << sizeof(int) << endl; // 4
     return 0;
 }
